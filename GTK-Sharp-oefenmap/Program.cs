0ede18202a2b3296bc298357d0472b764cddd51a
Gert <gert@gepe-biljarts.be>
2015-03-26 23:50:08 +0100

    Added comments

--- GTK-Sharp-oefenmap/Program.cs
+++ GTK-Sharp-oefenmap/Program.cs
@@ -7,13 +7,18 @@ namespace GTKSharpoefenmap
 	{
 		static Widget xpm_label_box(string xpm_filename, string label_text)
 		{
+
+			// Create box for image and label
 			HBox box = new HBox (false, 0);
 			box.BorderWidth = 2;
 
+			// Including the image 
 			Gtk.Image image = new Gtk.Image (xpm_filename);
 
+			// Create label of button
 			Label label = new Label (label_text);
 
+			// packing the image and the label
 			box.PackStart (image, false, false, 3);
 			box.PackStart (label, false, false, 3);
 
@@ -23,11 +28,13 @@ namespace GTKSharpoefenmap
 			return box;
 		}
 
+		// callback function for clicking the button (works only in console!)
 		static void callback(object obj,EventArgs args)
 		{
 			Console.WriteLine ("Hello again - cool button was pressed");
 		}
 
+		// collback for ending the program
 		static void delete_event (object obj, DeleteEventArgs args)
 		{
 			Application.Quit ();
@@ -36,20 +43,33 @@ namespace GTKSharpoefenmap
 		public static void Main (string[] args)
 		{
 			Application.Init ();
+
+			// create window
 			Window window = new Window ("Pixmap´d Buttons");
+
+			// set event for ending the program
 			window.DeleteEvent += delete_event;
 
 			window.BorderWidth = 10;
+
+			// creating the button
 			Button button = new Button ();
 
+			// click event
 			button.Clicked += callback;
 
+			// create box with label and image for inside button
 			Widget box = xpm_label_box ("info.xpm", "cool button");
-
 			box.Show ();
+
+			// add box t button
 			button.Add (box);
 			button.Show ();
+
+			// add button to window
 			window.Add (button);
+
+			// and show it all!
 			window.ShowAll ();
 			Application.Run ();
 		}
